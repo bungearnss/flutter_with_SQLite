7ae2f3f8abfc89fa8388dd28a79b9b1903c2cde7
bungearnss <love2554f@hotmail.com>
2023-01-12 10:04:30 +0700

    Add product service

---
+++ lib/src/services/product_service.dart
@@ -0,0 +1,25 @@
+import '../utils/helpers/db_helper.dart';
+import '../models/product.dart';
+
+class ProductService {
+  late DBHelper _dbHelper;
+
+  ProductService() {
+    _dbHelper = DBHelper();
+  }
+
+  Future<List<Product>> getProducts() async {
+    List<Map<String, dynamic>> productMaps = await _dbHelper.getData();
+
+    print("productMaps : $productMaps");
+
+    List<Product> products = [];
+    for (var map in productMaps) {
+      products.add(Product.fromMap(map));
+    }
+
+    print("products : $products");
+
+    return products;
+  }
+}
